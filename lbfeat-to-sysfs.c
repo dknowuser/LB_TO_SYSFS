0896ba8ca2fdd487cb5d77e6c377f1712258c9ed
dknowuser <maro4uk@yandex.ru>
2019-08-09 19:37:18 +0300

    Some fixes

--- lbfeat-to-sysfs.c
+++ lbfeat-to-sysfs.c
@@ -38,12 +38,15 @@ unsigned get_shift_by_attrib_name(const char *attrib_name);
 
 // Module device table
 static const struct of_device_id lb_base_sysfs_ids[] = {
-	{ .compatible = "stcmtk,lbfeat-to-sysfs", },
+	{ .compatible = "stcmtk,lbfeat-to-sysfs-0", },
 	{ },
 };
 
+// Uncomment it to enable autoloading
+// MODULE_DEVICE_TABLE(of, lb_sysfs_ids);
+
 static const char *LB_BASE_CLASS_NAME			= "smart_lb";
-static const char *LB_BASE_DEVICE_NAME			= "lb";
+static const char *LB_BASE_DEVICE_NAME			= "lb_";
 static const char *LB_BASE_CR_LB_EN_NAME 		= "LB_BASE_CR_LB_EN";
 static const char *LB_BASE_CR_MAC_SWAP_EN_NAME 		= "LB_BASE_CR_MAC_SWAP_EN";
 static const char *LB_BASE_CR_IP_SWAP_EN_NAME 		= "LB_BASE_CR_IP_SWAP_EN";
@@ -52,11 +55,9 @@ static const char *LB_BASE_CR_PASSTHROUGH_EN_NAME 	= "LB_BASE_CR_PASSTHROUGH_EN"
 static const char *LB_BASE_CR_PASSTHROUGH_MODE_NAME 	= "LB_BASE_CR_PASSTHROUGH_MODE";
 static const char *LB_BASE_CR_LB_L1_EN_NAME 		= "LB_BASE_CR_LB_L1_EN";
 
-// Uncomment it to enable autoloading
-// MODULE_DEVICE_TABLE(of, lb_sysfs_ids);
-
 static struct device *lb_base_sysfs_device;
 static struct regmap *regmap = NULL;
+static struct fpga_feature *lb_base_feat;
 
 static struct class *cl;
 static struct attribute attrib;
@@ -75,9 +76,28 @@ ssize_t show_lb_base(struct device *dev, struct device_attribute *attr,
 {
 	int err;
 	unsigned data = 0;
+	uint32_t port_number;
+	uint16_t cr_offset;
 	unsigned shift = get_shift_by_attrib_name(attr->attr.name);
 
-	err = regmap_read(regmap, FPGA_FEATURE_LB_BASE_CR, &data);
+	/*err = device_property_read_u32(lb_base_sysfs_device, "PORT", &port_number);
+	if(err) {
+		dev_err(dev, "Error reading LB_BASE feature port property from Device Tree\n");
+		return snprintf(buf, PAGE_SIZE, "Error reading LB_BASE feature port property from Device Tree\n");
+	};*/
+
+	//of_property_read_u32(dev->of_node, "port", &port_number);
+	
+	//const char *label = of_get_property(dev->of_node, "label", NULL);
+	//if(label)
+	if(dev->of_node)
+		return snprintf(buf, PAGE_SIZE, dev->init_name);
+
+
+	cr_offset = grif_fpga_feature_cr_base_on_port(lb_base_feat, port_number);
+	dev_info(dev, "port_number = %d cr_offset = %d\n", port_number, cr_offset);
+
+	err = regmap_read(regmap, cr_offset, &data);
 	if(err) {
 		dev_err(dev, "Error reading LB_BASE feature control register\n");
 		return snprintf(buf, PAGE_SIZE, "Error reading LB_BASE feature control register\n");
@@ -125,7 +145,7 @@ ssize_t store_lb_base(struct device *dev, struct device_attribute *attr,
 	return size;
 };
 
-static DEVICE_ATTR(LB_BASE_CR, S_IRUGO|S_IWUSR, show_lb_base, store_lb_base);
+static DEVICE_ATTR(LB_BASE_CR_LB_EN, S_IRUGO|S_IWUSR, show_lb_base, store_lb_base);
 
 //-----------------------------------------------------------------------------
 
@@ -134,7 +154,9 @@ static DEVICE_ATTR(LB_BASE_CR, S_IRUGO|S_IWUSR, show_lb_base, store_lb_base);
 static int lb_base_sysfs_probe(struct platform_device *pdev)
 {
 	struct grif_fpga *g;
-	struct fpga_feature *lb_base_feat;
+	//struct fpga_feature *lb_base_feat;
+	uint32_t port_number;
+	//char dev_name[5];
 
 	dev_info(&pdev->dev, "Smart LB to sysfs module probing procedure starts\n");
 	g = get_grif_fpga(pdev->dev.of_node);
@@ -152,6 +174,18 @@ static int lb_base_sysfs_probe(struct platform_device *pdev)
 	lb_base_feat = grif_fpga_get_feature(g, FPGA_FEATURE_LB_BASE);
 	if(!lb_base_feat) {
 		dev_err(&pdev->dev, "Couldn't get LB_BASE feature\n");
+		return -ENODEV;
+	};
+
+	if(!of_find_property(pdev->dev.of_node, "port", NULL)) {
+		dev_err(&pdev->dev, "Port property is not presented in Device Tree\n");
+		return -ENODEV;
+	};
+
+	of_property_read_u32(pdev->dev.of_node, "port", &port_number);
+	dev_info(&pdev->dev, "port_number = %d\n", port_number);
+	if(!((lb_base_feat->port_mask >> port_number) & 1)) {
+		dev_err(&pdev->dev, "Port %d is not set in LB_BASE feature port mask\n", port_number);
 		return -ENODEV;
 	};
 
